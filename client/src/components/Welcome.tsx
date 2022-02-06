61671bb02bbe78f9ae4cf3d13a945de764f87f43
Daniel Unapanta <dunapanta@outlook.es>
2022-02-05 22:59:24 -0500

    Begin with welcome page

--- client/src/components/Welcome.tsx
+++ client/src/components/Welcome.tsx
@@ -1,7 +1,22 @@
 import React from "react";
+import { AiFillPayCircle } from "react-icons/ai";
+import { SiEthereum } from "react-icons/si";
+import { BsInfoCircle } from "react-icons/bs";
+
+import { Loader } from "./";
 
 const Welcome = () => {
-  return <h1>Welcome</h1>;
+  return (
+    <div className="flex items-center justify-center w-full">
+      <div className="flex flex-col items-start justify-between px-4 py-12 md:flex-row md:p-20">
+        <div className="flex flex-col justify-start flex-1 md:mr-10">
+          <h1 className="py-1 text-3xl text-white sm:text-5xl text-gradient">
+            Enviar Crypto <br /> alrededor del mundo
+          </h1>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Welcome;
