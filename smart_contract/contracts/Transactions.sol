4de264d845593c058b28125745d6755d2917985d
Daniel Unapanta <dunapanta@outlook.es>
2022-03-12 20:30:12 -0500

    Implement solidity ethereum smart contract

--- smart_contract/contracts/Transactions.sol
+++ smart_contract/contracts/Transactions.sol
@@ -5,7 +5,7 @@ pragma solidity ^0.8.8;
 //Create a contract
 contract Transactions {
     //Solidity definition variables
-    uint256 transactionCounter;
+    uint256 transactionCount;
 
     event Transfer(address from, address receiver, uint amount, string message, uint256 timestamp, string keyword);
 
@@ -20,17 +20,21 @@ contract Transactions {
 
     TransferStruct[] transactions;
 
-    function addToBlockchain() public {
+    function addToBlockchain(address payable receiver, uint amount, string memory message, string memory keyword) public {
+        transactionCount +=1;
+        transactions.push(TransferStruct(msg.sender, receiver, amount, message, block.timestamp, keyword));
 
+        // to make the transfer we need to emmit an event
+        emit Transfer(msg.sender, receiver, amount, message, block.timestamp, keyword);
     }
 
     function getAllTransactions() public view returns (TransferStruct[] memory) {
-        //return transactions;
+        return transactions;
 
     }
 
     function getTransactionCount() public view returns (uint256) {
-        //return transactionCounter;
+        return transactionCount;
     
 
     }
